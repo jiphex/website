73f51396253b7213a51c163a1dc1c59e642cf49d
James Hannah <james.hannah@stfc.ac.uk>
2022-02-15 21:52:19 +0000

    Tweak headers CSP for CF Analytics, tweak JS
    - Allow CF insights JS to load
    - Remove JS console error caused by missing share button.

--- themes/impthink2021/assets/js/index.js
+++ themes/impthink2021/assets/js/index.js
@@ -1,8 +1,11 @@
-document.getElementById("sharebutton").addEventListener("click", async () => {
-	try {
-		await navigator.share({ title: "{{ $page.title }}", url: "" });
-		console.log("Data was shared successfully");
-	} catch (err) {
-		console.error("Share failed:", err.message);
-	}
-});
+var el = document.getElementById("sharebutton");
+if(el != null) {
+	el.addEventListener("click", async () => {
+		try {
+			await navigator.share({ title: "{{ $page.title }}", url: "" });
+			console.log("Data was shared successfully");
+		} catch (err) {
+			console.error("Share failed:", err.message);
+		}
+	});
+};
